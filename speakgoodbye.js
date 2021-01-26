101ebf4c6906512bd1bc65cd0b5b2a7e16287a46
Lavezur <artemeremenko.m@yandex.ru>
2021-01-26 23:32:43 +0300

    GG

--- speakgoodbye.js
+++ speakgoodbye.js
@@ -1,8 +1,8 @@
 (function(window) {
-    var byeSpeaker = new Object();
+    var byeUser = new Object();
     var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
+    byeUser.speak = function speak(name) {
         console.log(speakWord + " " + name);
     };
-    window.byeSpeaker = byeSpeaker;
+    window.byeUser = byeUser;
 })(window);
